dba72a2c3b4824e49d895b222d1bf9a6a7f9eb9f
Aryanverma09 <iamaryanverma5555@gmial.com>
2025-01-04 22:04:54 +0530

    little bit modify

--- loco.js
+++ loco.js
@@ -3,3 +3,7 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+
+document.body.style.overflowY = "scroll"; /* Enable scrolling */
+document.body.style.scrollbarWidth = "none"; /* Hide scrollbar in supported browsers */
